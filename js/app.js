760f05e988bc8a19d20d2d95d72774b69d695c18
Anna Górzanowska <anna.gorzanowska@gmail.com>
2017-11-23 16:07:41 +0100

    PM10% and PM2.5% added :dash:

--- js/app.js
+++ js/app.js
@@ -12,25 +12,33 @@ $.ajax({
 
     success: function (response) {
         console.log(response);
+        const pm10standard24h = 50;
+        const pm25standard24h = 25;
         const pm25 = response.pm25;
         const pm10 = response.pm10;
+        const pm10perc = (response.pm10/pm10standard24h*100).toFixed(2);
+        const pm25perc = (response.pm25/pm25standard24h*100).toFixed(2);
         const street = response.address.route;
         document.querySelector(".air").innerText = street + ', PM2.5: ' + pm25 + ', PM10: ' + pm10;
         var ctx = document.getElementById("myChart").getContext('2d');
         var myChart = new Chart(ctx, {
             type: 'bar',
             data: {
-                labels: ["PM10", "PM2.5"],
+                labels: ["PM10", "PM2.5", "PM10%", "PM2.5%"],
                 datasets: [{
                     label: 'μg/m3',
-                    data: [pm10, pm25],
+                    data: [pm10, pm25, pm10perc, pm25perc],
                     backgroundColor: [
                         'rgba(255, 99, 132, 0.2)',
                         'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)'
                     ],
                     borderColor: [
                         'rgba(255,99,132,1)',
                         'rgba(54, 162, 235, 1)',
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)'
                     ],
                     borderWidth: 1
                 }]
