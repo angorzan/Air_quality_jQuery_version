13ee4563c9bf99b73e77e342e1a4ea15f728ca47
Anna Górzanowska <anna.gorzanowska@gmail.com>
2017-11-27 22:16:21 +0100

    Second chart added

--- js/app.js
+++ js/app.js
@@ -3,8 +3,8 @@ document.addEventListener('DOMContentLoaded', () => {
 button.addEventListener('click', () => {
     navigator.geolocation.getCurrentPosition(position => {
     console.log(position);
-var lat = position.coords.latitude;
-var lon = position.coords.longitude;
+var lat = position.coords.latitude.toFixed(2);
+var lon = position.coords.longitude.toFixed(2);
 document.querySelector(".position").innerText = 'Your position: ' + lat + ', ' + lon;
 $.ajax({
     url: 'https://airapi.airly.eu/v1/nearestSensor/measurements?latitude=' + lat + '&longitude=' + lon + '&maxDistance=1000',
@@ -27,22 +27,20 @@ $.ajax({
         document.querySelector(".air").innerText =  'Air quality: PM2.5: ' + pm25.toFixed(2) + 'µg/m3 ' + ', PM10: ' + pm10.toFixed(2) + 'µg/m3 ';
         document.querySelector(".distance").innerText = 'Distance: ' + distance + 'm';
         var ctx = document.getElementById("myChart").getContext('2d');
+        var ctx2 = document.getElementById("my_percentage_Chart").getContext('2d');
         var myChart = new Chart(ctx, {
             type: 'bar',
             data: {
-                labels: ["PM10", "PM2.5", "PM10%", "PM2.5%"],
+                labels: ["PM10", "PM2.5"],
                 datasets: [{
                     label: 'μg/m3',
                     data: [pm10, pm25, pm10perc, pm25perc],
                     backgroundColor: [
                         'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 99, 132, 0.2)',
                         'rgba(54, 162, 235, 0.2)'
+
                     ],
                     borderColor: [
-                        'rgba(255,99,132,1)',
-                        'rgba(54, 162, 235, 1)',
                         'rgba(255,99,132,1)',
                         'rgba(54, 162, 235, 1)'
                     ],
@@ -59,6 +57,37 @@ $.ajax({
                 }
             }
         });
+
+
+        var my_percentage_Chart = new Chart(ctx2, {
+            type: 'bar',
+            data: {
+                labels: ["PM10%", "PM2.5%"],
+                datasets: [{
+                    label: '%',
+                    data: [pm10perc, pm25perc],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+
     }
 });
 });
